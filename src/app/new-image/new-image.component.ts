7f180a4905e0dd64dcc3474b53946e23381a5c27
Aung Myo Kyaw <amk.maeu@gmail.com>
2017-04-25 17:05:45 +0630

    fix bugs

--- src/app/new-image/new-image.component.ts
+++ src/app/new-image/new-image.component.ts
@@ -22,8 +22,8 @@ export class NewImageComponent implements OnInit {
 
   onchangeHandler(imgUrl){
     let isUrl = /^https?:\/\//.test(imgUrl);
-    let url = isUrl ? imgUrl : 'http://localhost';
-    this.app.isImgBroken(imgUrl)
+    let url = isUrl ? imgUrl : 'http://placehold.it/350x100?text=Your+image+link+is+broken!';
+    this.app.isImgBroken(url)
       .subscribe(res=>{
         this.imageUrl = isUrl ? imgUrl : "http://placehold.it/350x100?text=Your+image+link+is+broken!";
       },err=>{
